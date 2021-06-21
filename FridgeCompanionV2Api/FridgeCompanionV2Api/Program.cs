34a1fbd253542644bb4b4f9ae247eb7056715895
ayuub2 <ayuubadan@outlook.com>
2021-06-21 19:16:21 +0100

    added url

--- FridgeCompanionV2Api/FridgeCompanionV2Api/Program.cs
+++ FridgeCompanionV2Api/FridgeCompanionV2Api/Program.cs
@@ -21,6 +21,7 @@ namespace FridgeCompanionV2Api
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
+                    webBuilder.UseUrls("http://0.0.0.0:5000/");
                 });
     }
 }
