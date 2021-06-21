0e787c4226b1924a9321c5d321a254e4efda5420
ayuub2 <ayuubadan@outlook.com>
2021-06-21 16:58:18 +0100

    added build files

---
+++ FridgeCompanionV2Api/FridgeCompanionV2Api/deploy-envs.sh
@@ -0,0 +1,10 @@
+  
+#!/bin/bash
+
+# Set variables
+export IMAGE_NAME=netcoreapps-fridgecompanion-v2-api
+export IMAGE_VERSION=latest
+
+export AWS_DEFAULT_REGION=eu-west-1
+export AWS_ECS_CLUSTER_NAME=FridgeCompanion
+export AWS_VIRTUAL_HOST=fridgecompanion.co.uk
