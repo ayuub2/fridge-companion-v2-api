135616f70ff2b3c709617268a28067fa33a9ac9b
ayuub2 <ayuubadan@outlook.com>
2021-06-28 20:37:55 +0100

    v2 base path

--- FridgeCompanionV2Api/FridgeCompanionV2Api/Startup.cs
+++ FridgeCompanionV2Api/FridgeCompanionV2Api/Startup.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -40,7 +41,7 @@ namespace FridgeCompanionV2Api
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
-
+            app.UsePathBase(new PathString("/v2"));
             app.UseHttpsRedirection();
             app.UseStaticFiles();
             app.UseRouting();
