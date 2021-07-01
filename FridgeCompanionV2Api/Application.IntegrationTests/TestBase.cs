88a95189eb2a2eb9063b8efe0765060361b33fc3
ayuub2 <ayuubadan@outlook.com>
2021-07-01 17:47:01 +0100

    added testing

---
+++ FridgeCompanionV2Api/Application.IntegrationTests/TestBase.cs
@@ -0,0 +1,16 @@
+﻿using NUnit.Framework;
+using System.Threading.Tasks;
+
+namespace FridgeCompanionV2Api.Application.IntegrationTests
+{
+    using static Testing;
+
+    public class TestBase
+    {
+        [SetUp]
+        public async Task TestSetUp()
+        {
+            await ResetState();
+        }
+    }
+}
