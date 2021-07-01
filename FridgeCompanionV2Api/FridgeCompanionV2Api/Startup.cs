98bb2278444ec2e6fe71316e67de4d834fec4627
ayuub2 <ayuubadan@outlook.com>
2021-07-01 15:56:15 +0100

    added new swagger plus updated api version

--- FridgeCompanionV2Api/FridgeCompanionV2Api/Startup.cs
+++ FridgeCompanionV2Api/FridgeCompanionV2Api/Startup.cs
@@ -2,9 +2,12 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -24,7 +27,12 @@ namespace FridgeCompanionV2Api
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddRazorPages();
+
+            services.AddControllers();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "FridgeCompanionV2Api", Version = "v1" });
+            });
             services.AddHealthChecks();
         }
 
@@ -34,24 +42,19 @@ namespace FridgeCompanionV2Api
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-            }
-            else
-            {
-                app.UseExceptionHandler("/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "FridgeCompanionV2Api v1"));
             }
             app.UsePathBase(new PathString("/v2"));
             app.UseHttpsRedirection();
-            app.UseStaticFiles();
+
             app.UseRouting();
 
             app.UseAuthorization();
 
-
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapRazorPages();
+                endpoints.MapControllers();
                 endpoints.MapHealthChecks("/health");
             });
         }
