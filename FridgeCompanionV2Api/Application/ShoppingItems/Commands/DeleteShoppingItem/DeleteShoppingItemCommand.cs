f23a32bd29d9f81f9ade087f05e2a60715353377
ayuub2 <ayuubadan@outlook.com>
2021-07-24 15:07:38 +0100

    Added delete shopping item command, also update exceptions

---
+++ FridgeCompanionV2Api/Application/ShoppingItems/Commands/DeleteShoppingItem/DeleteShoppingItemCommand.cs
@@ -0,0 +1,15 @@
+﻿using FridgeCompanionV2Api.Application.Common.Models;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FridgeCompanionV2Api.Application.ShoppingItems.Commands.CreateShoppingItem
+{
+    public class DeleteShoppingItemCommand : IRequest<ShoppingItemDto>
+    {
+        public string UserId { get; set; }
+        public Guid Id { get; set; }
+    }
+}
