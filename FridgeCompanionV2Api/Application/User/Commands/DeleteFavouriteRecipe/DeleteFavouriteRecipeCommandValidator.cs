08ba6f2c099b6173b54f32c5ada0c3e8e74f8a7b
ayuub2 <ayuubadan@outlook.com>
2021-10-05 19:45:01 +0100

    - implement delete favourite recipe endpoint
    - add made recipes endpoint

---
+++ FridgeCompanionV2Api/Application/User/Commands/DeleteFavouriteRecipe/DeleteFavouriteRecipeCommandValidator.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FridgeCompanionV2Api.Application.User.Commands.DeleteFavouriteRecipe
+{
+    public class DeleteFavouriteRecipeCommandValidator
+    {
+    }
+}
