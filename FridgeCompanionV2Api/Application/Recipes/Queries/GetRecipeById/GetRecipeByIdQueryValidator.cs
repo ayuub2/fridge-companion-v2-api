b27e3079e32d8dffdaaa2d84f522af531f489200
ayuub2 <ayuubadan@outlook.com>
2022-03-27 21:45:28 +0100

    added get recipe by id

---
+++ FridgeCompanionV2Api/Application/Recipes/Queries/GetRecipeById/GetRecipeByIdQueryValidator.cs
@@ -0,0 +1,19 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FridgeCompanionV2Api.Application.Recipes.Queries.GetRecipeById
+{
+    public class GetRecipeByIdQueryValidator : AbstractValidator<GetRecipeByIdQuery>
+    {
+        public GetRecipeByIdQueryValidator()
+        {
+            RuleFor(x => x.UserId)
+                .NotEmpty().WithMessage("User is not authorized.");
+            RuleFor(x => x.RecipeId)
+                .NotEmpty().WithMessage("Recipe id not supplied.");
+        }
+    }
+}
