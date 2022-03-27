3a0e8863f3ff6315d5e79fdbbde50d308a418dc0
ayuub2 <ayuubadan@outlook.com>
2022-03-27 14:37:25 +0100

    added serving size

---
+++ FridgeCompanionV2Api/Application/Recipes/Queries/GetRecipeByServingSize/GetRecipeByServingSizeQuery.cs
@@ -0,0 +1,16 @@
+﻿using FridgeCompanionV2Api.Application.Common.Models;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FridgeCompanionV2Api.Application.Recipes.Queries.GetRecipeByServingSize
+{
+    public class GetRecipeByServingSizeQuery : IRequest<RecipeDto>
+    {
+        public string UserId { get; set; }
+        public int RecipeId { get; set; }
+        public int ServingSize { get; set; }
+    }
+}
---
+++ FridgeCompanionV2Api/Application/Recipes/Queries/GetRecipeByServingSize/GetRecipeByServingSizeQueryValidator.cs
@@ -0,0 +1,22 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FridgeCompanionV2Api.Application.Recipes.Queries.GetRecipeByServingSize
+{
+    public class GetRecipeByServingSizeQueryValidator : AbstractValidator<GetRecipeByServingSizeQuery>
+    {
+        public GetRecipeByServingSizeQueryValidator()
+        {
+            RuleFor(x => x.UserId)
+                .NotEmpty().WithMessage("User is not authorized.");
+            RuleFor(x => x.RecipeId)
+                .NotEmpty().WithMessage("Recipe id not supplied.");
+            RuleFor(x => x.ServingSize)
+                .NotEmpty().WithMessage("Serving size is not supplied.");
+
+        }
+    }
+}
